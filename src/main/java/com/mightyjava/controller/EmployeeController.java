1cdf7ff39c1910a367d5f39517cad2e7553c1336
Anthony <thony10.rc.c@gmail.com>
2020-11-01 15:30:24 -0500

    AC: añadiendo proyecto

---
+++ src/main/java/com/mightyjava/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package com.mightyjava.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import com.mightyjava.modal.Employee;
+import com.mightyjava.modal.ReCaptchaResponse;
+import com.mightyjava.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+	
+	private String message;
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${recaptcha.url}")
+	private String recaptchaURl;
+
+	@Value("${recaptcha.secret}")
+	private String recaptchaSECRET;
+	
+	@GetMapping("/")
+	public String add(Model model) {
+		model.addAttribute("message", message);
+		model.addAttribute("employee", new Employee());
+		return "add";
+	}
+	
+	@PostMapping("/save")
+	public String save(@ModelAttribute("employee") Employee employee, @RequestParam(name="g-recaptcha-response") String captchaResponse) {
+		// String params = "?secret=6Lcc0HwUAAAAAE95NcYpjnL1eQ-EuIpViecOpWRQ&response="+captchaResponse;
+		String params = "?secret="+ recaptchaSECRET +"&response="+captchaResponse;
+
+		ReCaptchaResponse reCaptchaResponse = restTemplate.exchange(recaptchaURl+params, HttpMethod.POST, null, ReCaptchaResponse.class).getBody();
+		if(reCaptchaResponse.isSuccess()) {
+			employeeService.add(employee);
+			return "redirect:/list";
+		} else {
+			message = "Please verify captcha";
+			return "redirect:/";
+		}
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		message = null;
+		model.addAttribute("employees", employeeService.employees());
+		return "list";
+	}
+}
